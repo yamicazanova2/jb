17bf34dad506d4179cc38323cec04d76e9ed51ca
yamicazanova2 <yamicazanova2@gmail.com>
2021-04-10 14:20:34 +0100

    EDIT

---
+++ experimental/702/changelog.js
@@ -0,0 +1,20 @@
+let changes=`2021-04-09: Added GoldHEN 1.1.
+2021-04-08: Implemented online wallpapers.
+2021-04-07:
+  - Improved logging.
+  - Added average attempts and time.
+  - Implemented offline wallpaper.
+2021-04-04:
+  - Implemented Jailbreak history and Settings.
+  - Firmware check now ignores spoof.
+2021-03-30:
+  - Fixed PS4Debug not loading.
+  - Tidied up the jailbreak output.
+2021-03-29:
+  - Fixed not going back to main page after successful jailbreak.
+  - The WebKit exploit now resumes after error "Failed to setup a relative read primitive".
+2021-03-28: Fixed regular HEN payload not being regular HEN.
+2021-03-27: Added firmware safety checks.
+2021-03-26: Added ToDEX payload.`;
+let date="2021-04-09";
+let build="1";
