ecd52388dc10b9e27810233db260f9652fffbfc2
yamicazanova2 <yamicazanova2@gmail.com>
2021-04-10 14:17:06 +0100

    Edit

---
+++ 75x/changelog.js
@@ -0,0 +1,13 @@
+let changes=`2021-04-09: Added GoldHEN 1.1.
+2021-04-04:
+  - Firmware check now ignores spoof.
+  - Added sleirsgoevy Linux payloads.
+  - Updated fixed Nazky Linux payloads.
+  - Updated Mira.
+2021-03-29:
+  - Fixed not going back to main page after successful jailbreak.
+  - The WebKit exploit now resumes after error "Failed to setup a relative read primitive".
+2021-03-28: Added Linux payloads.
+2021-03-27: Added firmware safety checks.`;
+let date="2021-04-09";
+let build="2";
